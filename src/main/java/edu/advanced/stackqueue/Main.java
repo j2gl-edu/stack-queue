22d7278c349633e423c71c16bab8a2af4b6b8037
Juan J. Garcia <j2gl.edu@gmail.com>
2019-03-03 22:39:41 -0600

    Ready for queue exercise

--- src/main/java/edu/advanced/stackqueue/Main.java
+++ src/main/java/edu/advanced/stackqueue/Main.java
@@ -3,7 +3,8 @@ package edu.advanced.stackqueue;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Main");
+        System.out.println("Queue Main");
+        // TODO Write your code here.
     }
 
 }
